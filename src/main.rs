027b48e76ea6dacce7f575193e22a777dc9bf771
TAKAHASHI Satoshi <hikobae@gmail.com>
2025-05-06 22:55:10 +0900

    標準出力の高速化
    参考: <https://keens.github.io/blog/2017/10/05/rustdekousokunahyoujunshutsuryoku/>

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ use std::io;
 mod quote;
 
 fn main() {
+    let mut stdout = io::BufWriter::new(io::stdout().lock());
+    let mut stderr = io::BufWriter::new(io::stderr().lock());
     let args: Vec<String> = env::args().collect();
     if args.len() <= 1 {
-        quote::quote(io::stdin().lock());
+        quote::quote(io::stdin().lock(), &mut stdout);
     } else {
-        quote::quote_files(&args[1..]);
+        quote::quote_files(&args[1..], &mut stdout, &mut stderr);
     }
 }
--- src/quote.rs
+++ src/quote.rs
@@ -1,20 +1,34 @@
 use std::fs;
 use std::io;
-use std::io::BufRead;
 
 const QUOTE_MARK: &str = ">";
 
-pub fn quote<T: BufRead>(reader: T) {
+pub fn quote<R, T>(reader: R, stdout: &mut T)
+where
+    R: io::BufRead,
+    T: io::Write,
+{
     for line in reader.lines() {
-        println!("{}", quote_line(&line.unwrap()));
+        stdout
+            .write_all(quote_line(&line.unwrap()).as_bytes())
+            .unwrap();
+        stdout.write_all("\n".as_bytes()).unwrap();
     }
+    stdout.flush().unwrap();
 }
 
-pub fn quote_files(paths: &[String]) {
+pub fn quote_files<T, U>(paths: &[String], stdout: &mut T, stderr: &mut U)
+where
+    T: io::Write,
+    U: io::Write,
+{
     for arg in paths {
         fs::File::open(arg)
-            .map(|file| quote(io::BufReader::new(file)))
-            .unwrap_or_else(|error| eprintln!("{}", error));
+            .map(|file| quote(io::BufReader::new(file), stdout))
+            .unwrap_or_else(|error| {
+                stderr.write_all(error.to_string().as_bytes()).unwrap();
+                stderr.flush().unwrap();
+            });
     }
 }
 
