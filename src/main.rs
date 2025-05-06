ba7599ad8cf203792ca49ee9c3754cabf1422eb6
TAKAHASHI Satoshi <hikobae@gmail.com>
2025-05-06 20:34:43 +0900

    モジュール名を mail から quote に変更

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,16 @@ use std::env;
 use std::fs;
 use std::io;
 
-mod mail;
+mod quote;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() <= 1 {
-        mail::quote(io::stdin().lock());
+        quote::quote(io::stdin().lock());
     } else {
         for arg in &args[1..] {
             fs::File::open(arg)
-                .map(|file| mail::quote(io::BufReader::new(file)))
+                .map(|file| quote::quote(io::BufReader::new(file)))
                 .unwrap_or_else(|error| eprintln!("{}", error));
         }
     }
